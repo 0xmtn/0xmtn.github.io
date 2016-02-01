1c1a5aa314938e214f712caf11471ac458d1ff1b
Metin Emenullahi <metin@mtndesign.net>
2016-02-01 15:17:40 +0200

    Flush

--- js/globals.js
+++
@@ -1,7 +0,0 @@
-(function () {
-  'use strict';
-
-  window.USERNAME = '0xmtn'; // github username
-  window.REPO = '0xmtn.github.io'; // github repository
-  window.TOKEN = '423acb04e2b1d48a94197b59c6a8c55320393275'; // github app access token
-}());
