71e3382c21c2d8ba0e1ef51828e7b02b4ea9b175
Metin Emenullahi <metin@mtndesign.net>
2016-02-01 15:02:07 +0200

    Experiment I - authorization and gettings issues

---
+++ js/globals.js
@@ -0,0 +1,7 @@
+(function () {
+  'use strict';
+
+  window.USERNAME = '0xmtn'; // github username
+  window.REPO = '0xmtn.github.io'; // github repository
+  window.TOKEN = 'e70eebb2b683a22e6b4403f8fa3423927404068b'; // github app access token
+}());
