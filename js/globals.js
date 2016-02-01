a1a6caa5bfc854abf9dc6a83aca471a766f40416
Metin Emenullahi <metin@mtndesign.net>
2016-02-01 15:16:06 +0200

    Update globals.js

--- js/globals.js
+++ js/globals.js
@@ -3,5 +3,5 @@
 
   window.USERNAME = '0xmtn'; // github username
   window.REPO = '0xmtn.github.io'; // github repository
-  window.TOKEN = 'f440631fb7c0d62a8f28218c4bd9451be49c92c3'; // github app access token
+  window.TOKEN = '423acb04e2b1d48a94197b59c6a8c55320393275'; // github app access token
 }());
